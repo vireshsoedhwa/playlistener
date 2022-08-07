ac363bf4db0b4750100f990927a9dbfe212be947
Viresh Soedwa <vireshsoedhwa@gmail.com>
2022-08-06 20:15:50 -0700

    add new models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.db.models import Deferrable, UniqueConstraint
@@ -7,11 +8,31 @@ from django.conf import settings
 
 from django_q.tasks import async_task, result, fetch
 
-import re
-
 
 def file_directory_path(instance, filename):
     return ('{0}/{1}').format(instance.id, filename)
+
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Artist(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return str(self.name)
 
 
 class MediaResource(models.Model):
@@ -22,23 +43,21 @@ class MediaResource(models.Model):
                                  null=True,
                                  blank=True,
                                  max_length=500)
+    original_duration = models.DurationField()
     audiofile_432 = models.FileField(upload_to=file_directory_path,
-                                 null=True,
-                                 blank=True,
-                                 max_length=500)
+                                     null=True,
+                                     blank=True,
+                                     max_length=500)
+
     md5_generated = models.TextField(max_length=32, null=True, blank=True)
     genre = models.TextField(max_length=100, null=True, blank=True)
-    artist = models.TextField(max_length=100, null=True, blank=True)
+    artists = models.ManyToManyField(Artist)
+    tags = models.ManyToManyField(Tag)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id) + " : " + str(self.title)
 
-# signal for deleting
-@receiver(post_delete, sender=MediaResource, dispatch_uid="delete_yt_archive_record")
-def delete_record(sender, instance, **kwargs):
-
-    print(f"Deleted ID:{instance.id}")
 
 class YoutubeMediaResource(models.Model):
     youtube_id = models.TextField(primary_key=True, max_length=200)
@@ -55,13 +74,14 @@ class YoutubeMediaResource(models.Model):
                                             default=False)
     busy = models.BooleanField(null=True, blank=True, default=False)
     downloadprogress = models.DecimalField(max_digits=3, decimal_places=0, blank=True,
-                                   default=0)
+                                           default=0)
     eta = models.DecimalField(max_digits=5, decimal_places=0, blank=True,
                               default=0)
     elapsed = models.DecimalField(max_digits=5, decimal_places=0, blank=True,
                                   default=0)
     speed = models.DecimalField(max_digits=10, decimal_places=0, blank=True,
                                 default=0)
+
 
 @receiver(post_save, sender=YoutubeMediaResource, dispatch_uid="create_mediaresource")
 def checkdownload(sender, instance, created, raw, using, update_fields, **kwargs):
@@ -76,4 +96,11 @@ def checkdownload(sender, instance, created, raw, using, update_fields, **kwargs
     else:
         pass
         # TODO retry download here on user request
-    
+
+# signal for deleting
+
+
+@receiver(post_delete, sender=MediaResource, dispatch_uid="delete_yt_archive_record")
+def delete_record(sender, instance, **kwargs):
+
+    print(f"Deleted ID:{instance.id}")
