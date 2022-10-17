bb93660e0a9623e493f946a0d57fa8aec254c3ee
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2022-10-17 01:27:03 -0700

    enable admin

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import MediaResource, Artist, Tag
-
+admin.site.site_header = "PLAPI Admin"
+admin.site.site_title = "PLAPI site admin"
 admin.site.register(MediaResource)
 admin.site.register(Artist)
 admin.site.register(Tag)
