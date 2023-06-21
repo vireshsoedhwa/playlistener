7cf8ff1a0531d5fe94d826de5cf75169f43bc8df
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2023-06-21 01:27:05 -0700

    cleanup

--- app/tasks.py
+++ app/tasks.py
@@ -2,16 +2,22 @@
 
 from .models import MediaResource
 import os
+import glob
 from celery import shared_task
 from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
+from .serializers import MediaResourceSerializer
+
 @shared_task
 def count_items():
 
-    # for file in os.listdir("/mydir"):
-    logger.info("IT RRAN THE TASk Rannnn has started")
-    print("why no logs ")
+    root_dir = "/code/data/"
+    for filename in glob.iglob(root_dir + '**/*.mp3', recursive=True):
+        logger.info(filename)
+
+    # logger.info("IT RRAN THE TASk Rannnn has started")
+
     return MediaResource.objects.count()
 
 
